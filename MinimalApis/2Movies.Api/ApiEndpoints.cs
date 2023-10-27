4ad210ebc38e57ae845be9dbae502ef500d71c98
Zarrko <zacmwangi60@gmail.com>
2023-10-27 22:43:34 +0300

    Fix routing

--- MinimalApis/2Movies.Api/ApiEndpoints.cs
+++ MinimalApis/2Movies.Api/ApiEndpoints.cs
@@ -3,25 +3,24 @@ namespace _2Movies.Api;
 public static class ApiEndpoints
 {
     private const string ApiBase = "api";
-    private const string VersionBase = $"{ApiBase}";
+    
     public static class Movies
     {
-        private const string Base = $"{VersionBase}/movies";
+        private const string Base = $"{ApiBase}/movies";
 
-        public const string Create = Base;
+        public const string Create = $"{Base}/create";
         public const string Get = $"{Base}/{{idOrSlug}}";
         public const string GetAll = Base;
         public const string Update = $"{Base}/{{id:guid}}";
         public const string Delete = $"{Base}/{{id:guid}}";
-        
+
         public const string Rate = $"{Base}/{{id:guid}}/ratings";
         public const string DeleteRating = $"{Base}/{{id:guid}}/ratings";
-
     }
-
+    
     public static class Ratings
     {
-        private const string Base = $"{VersionBase}/ratings";
+        private const string Base = $"{ApiBase}/ratings";
 
         public const string GetUserRatings = $"{Base}/me";
     }
